89d23eeec8ccb9777d451009af8e5b9add043f43
Michel John <michel.john@hotmail.com>
2022-11-21 09:51:11 -0300

    setup pwa

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true
-};
+const isProd = process.env.NODE_ENV === 'production';
 
-// eslint-disable-next-line no-undef
-module.exports = nextConfig;
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  disable: !isProd
+});
+
+module.exports = withPWA({});
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -12,6 +12,8 @@ export default function App({ Component, pageProps }: AppProps) {
         <link rel="shortcut icon" href="/img/icon-512.png" />
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
 
+        <link rel="manifest" href="/manifest.json" />
+
         <meta
           name="description"
           content="Simple project boiler plate with TypeScript, React, NextJs, Styled Components"
